91a2e520867d6ec438674d877565fa66411f3657
Chan King To <chankingto@Chans-MacBook-Air.local>
2013-07-20 12:39:59 +0800

    edited by Eddie

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstWebpage::Application.config.secret_key_base = 'b3c5fa8de7fa9e27fd5482c3fce71568e3b2190dd7a1198047433a30fc9576cc893ca841f22d687138cfcbb6e4268b7731d3e62ebe7548445c2850dae6a465f9'
